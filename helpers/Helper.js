da07c66f83017825b6de50cc6b4cf24deeca3aa0
Mohamed Atef <atefhamosa@gmail.com>
2023-02-08 19:36:15 +0200

    Fix host fetch

--- helpers/Helper.js
+++ helpers/Helper.js
@@ -1,27 +1,25 @@
+const host = "http://localhost:3001";
+
 export const getStore = async () => {
-  const res = await fetch("http://localhost:3001/api/v1/stores/khaby");
+  const res = await fetch(host + "/api/v1/stores/khaby");
   const data = await res.json();
   return data;
 };
 
 export const getProduct = async (id, store) => {
-  const res = await fetch(
-    `http://localhost:3001/api/v1/products/${id}?store=${store}`
-  );
+  const res = await fetch(host + `/api/v1/products/${id}?store=${store}`);
   const data = await res.json();
   return data;
 };
 
 export const getProducts = async (store) => {
-  const res = await fetch(
-    `http://localhost:3001/api/v1/products?store=${store}`
-  );
+  const res = await fetch(host + `/api/v1/products?store=${store}`);
   const data = await res.json();
   return data;
 };
 
-export const getStaticProducts = async (store) => {
-  const res = await fetch(`http://localhost:3001/api/v1/products`, {
+export const getStaticProducts = async () => {
+  const res = await fetch(host + `/api/v1/products`, {
     method: "POST",
   });
   const data = await res.json();
