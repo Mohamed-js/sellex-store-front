ad1de043ce9f5383132f7cf90d27a4e443618450
Mohamed Atef <atefhamosa@gmail.com>
2023-02-21 22:58:35 +0200

    Update Sidebar.jsx

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -74,17 +74,17 @@ export default function Sidebar({
             </header>
             <ul
               style={{ color: `${store.options.navbar.links.color}` }}
-              className=" flex flex-col  w-full items-center px-6 text-gray-700 pr-2 sm:hidden "
+              className="flex flex-col w-full px-6 text-gray-700 pr-2 sm:hidden text-lg font-thin"
             >
               {store.options.navbar.links.home.exists && (
                 <Link
                   href={home ? "#" : "/"}
                   onClick={() => setSidebarOpened(false)}
-                  className="flex  hover:underline text-blue-600
-                  hover:text-green-800 
-                  visited:text-green-700"
+                  className="flex hover:underline
+                  hover:text-blue-800 
+                  "
                 >
-                  <li className="flex items-end mr-2 mb-5 ">
+                  <li className="flex mr-2 mb-5">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -99,11 +99,9 @@ export default function Sidebar({
                         d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
                       />
                     </svg>{" "}
-                    {store.options.navbar.links.with_text && (
-                      <span className="h-5">
-                        {store.options.navbar.links.home.text}
-                      </span>
-                    )}
+                    <span className="h-5">
+                      {store.options.navbar.links.home.text}
+                    </span>
                   </li>
                 </Link>
               )}
@@ -113,11 +111,11 @@ export default function Sidebar({
                   onClick={(e) => {
                     aboutScroll(e), setSidebarOpened(false);
                   }}
-                  className="cursor-pointer underline hover:no-underline
-                  text-blue-600 hover:text-blue-800 
-                  visited:text-red-600"
+                  className="cursor-pointer hover:underline
+                  text-black hover:text-blue-800 
+                  "
                 >
-                  <li className="flex items-end mr-2 mb-5">
+                  <li className="flex mr-2 mb-5">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -132,11 +130,10 @@ export default function Sidebar({
                         d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
                       />
                     </svg>
-                    {store.options.navbar.links.with_text && (
-                      <span className="h-5">
-                        {store.options.navbar.links.about.text}
-                      </span>
-                    )}
+
+                    <span className="h-5">
+                      {store.options.navbar.links.about.text}
+                    </span>
                   </li>
                 </Link>
               )}
@@ -146,11 +143,11 @@ export default function Sidebar({
                   onClick={(e) => {
                     contactScroll(e), setSidebarOpened(false);
                   }}
-                  className="cursor-pointer underline hover:no-underline
-                  text-blue-600 hover:text-blue-800 
-                  visited:text-purple-600"
+                  className="cursor-pointer hover:underline
+                  text-black hover:text-blue-800
+                  "
                 >
-                  <li className="flex items-end mr-2 mb-5">
+                  <li className="flex mr-2 mb-5">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -165,11 +162,10 @@ export default function Sidebar({
                         d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
                       />
                     </svg>
-                    {store.options.navbar.links.with_text && (
-                      <span className="h-5">
-                        {store.options.navbar.links.contact.text}
-                      </span>
-                    )}
+
+                    <span className="h-5">
+                      {store.options.navbar.links.contact.text}
+                    </span>
                   </li>
                 </Link>
               )}
